8a2dfd455828a2a8cb51592f3ae4d997e75342ce
dongstop <huangwudong2011@gmail.com>
2023-07-25 13:31:17 +0800

    Update PostLayout.tsx

--- src/layouts/PostLayout.tsx
+++ src/layouts/PostLayout.tsx
@@ -109,17 +109,17 @@ export default function PostLayout(props: Props) {
               <div className="prose max-w-none break-words pt-10 pb-8 dark:prose-dark">
                 {children}
               </div>
-              <div className="border-t border-gray-200 pt-6 pb-6 text-sm text-gray-700 dark:border-gray-700 dark:text-gray-300">
+{/*              <div className="border-t border-gray-200 pt-6 pb-6 text-sm text-gray-700 dark:border-gray-700 dark:text-gray-300">
                 <div className="mb-3">
-{/*                  <Image
+                  <Image
                     alt="runjs-cool"
                     width={384}
                     height={140}
                     src={`/static/images/runjs-cool.webp`} 底图
-                  />*/}
+                  />
                 </div>
-                {/*<span>adone</span>*/}
-              </div>
+                <span>adone</span>
+              </div>*/}
               <Comments frontMatter={frontMatter} />
             </div>
             <footer>
