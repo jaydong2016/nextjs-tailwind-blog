f8a46992411cc48b94b4f83168358c6fcc7fe866
dongstop <huangwudong2011@gmail.com>
2023-07-25 12:10:10 +0800

    up

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: '文章' },
   { href: '/tags', title: '标签' },
-  { href: '/kk', title: 'kk' },
+  { href: '/kkb', title: 'KKB' },
   { href: 'https://sbai.dongstop.link/', title: 'hapiGPT' },
   { href: '/about', title: '关于' },
 ]
